e87e16ba34a4c95b904a8d8c9dec5a649db90db9
LatoyaNixon <nixonlatoya.ln@gmail.com>
2021-10-07 16:22:13 -0400

    Completed the winner of game section in battlefield

--- battlefield.py
+++ battlefield.py
@@ -1,8 +1,12 @@
 from _typeshed import Self
+from dinosaur import Dinosaur
 from herd import Herd
 from fleet import Fleet
-import sys
-import logging
+from robot import Robots
+from weapon import Weapon
+
+
+
 
 class Battlefield():
     def __init__(self):
@@ -25,40 +29,56 @@ class Battlefield():
             self.display_robots()
 
         self.choose_dino()
+        self.choose_robot()
         self.battle()
-        self.dead
         self.winner_of_battle()
         self.end_of_game()
 
-    def display_welcome(self):
-    print("Welcome to Robots vs Dinosaurs!")
-
+        print("Welcome to robots vs dinosaur!")
+     
     def display_dino(self):
         index = 0
         for each in self.herd.herd_list:
             print(f"{index} {each.type} Power level -{each.attach_power}")
             index += 1
-
+     
     def choose_dino(self):
         self.dino_input = (input("/n Please select a Dinosaur\n"))
         for each in self.herd.herd_list:
-            if each == self.herd.herd.list[self.dino_input]:
-                return(self.herd.herd.list(self.dino_input.type))
+            if each == self.herd.herd_list[self.dino_input]:
+                return(self.herd.herd_list(self.dino_input.type))
              
                 
     def display_robot(self):
         index = 0
-        for each in self.fleet.fleet.list:
+        for each in self.fleet.fleet_list:
             print(f"{index} {each.type} Power level -{each.attach.power}")
             index +=1
-
+        
     def choose_robot(self):
         self.robot_input = (input("/n Please select a Robot\n"))
         for each in self.fleet.fleet_list:
-            if each == self.fleet.fleet.list[self.robot_input]:
-                return(self.fleet.fleet.list(self.robot_input.type))
+            if each == self.fleet.fleet_list[self.robot_input]:
+                return(self.fleet.fleet_list(self.robot_input.type))
 
-    def show_dino_opponent_options(self):
+    def battle(self):
+        pass
+
+
+
+
+    def winner_of_battle(self):
+        print("The winner has been declared")
+        if (self.herd.herd_list != 0):
+            print("The dinosaur has won the battle!")
+        if (self.fleet.fleet_list != 0):
+            print("The robot has won the battle!")
+        
+              
+
+
+
+
 
 
     
